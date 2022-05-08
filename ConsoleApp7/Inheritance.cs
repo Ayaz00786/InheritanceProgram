7fe4a140316a9ec1599edfcf47fbd473ff5fcbb6
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-05-08 12:00:26 +0530

    Add project files.

---
+++ ConsoleApp7/Inheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine("Employee Name: "+FirstName + " " + LastName + "\nEmployee Email: " + Email);
+        }
+    }
+   
+
+    public class FullTimeEmployee : Employee
+    {
+        public double YearlySalary { get; set; }
+
+        public  void FullEmployee()
+        {
+            Console.WriteLine("Employee Monthly Salary: " + YearlySalary/12 +"\nEmployee Yearly Salary: "+ YearlySalary);
+        }
+
+    }
+    public class PartTimeEmployee : Employee
+    {
+        public double HourlySalary { get; set; }
+        public int DailyWorkHour  {get; set;}
+         
+        public void PTEmployee()
+        {
+            Console.WriteLine("Employee Work Hour: "+DailyWorkHour +"\nEmployee Per Day Salary:"+HourlySalary*DailyWorkHour);
+        }
+    }
+
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee fullTime = new FullTimeEmployee();
+
+            Console.WriteLine("----------FullTime Employee Details----------");
+            fullTime.FirstName = "Ayaz";
+            fullTime.LastName = "Ahmad";
+            fullTime.Email = "Ayaz123@gmail.com";
+            fullTime.YearlySalary = 240000;
+            fullTime.PrintFullName();
+            fullTime.FullEmployee();
+
+
+            Console.WriteLine("\n------------PartTime Employee Details----------"); 
+            PartTimeEmployee partTime = new PartTimeEmployee();
+            partTime.FirstName = "Ayaz";
+            partTime.LastName = "Ahmad";
+            partTime.Email = "Ayaz123@gmail.com";
+            partTime.HourlySalary = 240;
+            partTime.DailyWorkHour = 5;
+            partTime.PrintFullName();
+            partTime.PTEmployee();  
+
+            
+
+        }
+    }
+}
